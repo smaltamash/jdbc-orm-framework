aa9731edb4150df383b50d354a079097d2a63064
Syed Mohd Altamash <44392139+smaltamash@users.noreply.github.com>
2019-08-17 13:27:22 +0530

    Update TestJdbcOrmFramework.java

--- src/com/jdbc/orm/test/TestJdbcOrmFramework.java
+++ src/com/jdbc/orm/test/TestJdbcOrmFramework.java
@@ -1,7 +1,6 @@
 package com.jdbc.orm.test;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.ArrayList;
 
 import com.jdbc.orm.JdbcOrmFramework;
 import com.jdbc.orm.Student;
@@ -13,66 +12,103 @@ public class TestJdbcOrmFramework {
 			jof.url="jdbc:oracle:thin:@localhost:1521:xe";
 			jof.userName="hr";
 			jof.pass="hr";
-			jof.createConnection(jof.connectivity, jof.url, jof.userName, jof.pass);
+			try{
+				jof.createConnection(jof.connectivity, jof.url, jof.userName, jof.pass);
+			}
+			catch(Exception e) {
+				System.out.println("Failed to Establish Connection");
+			}
 			System.out.println("Connection Established with Database Successfully");
 			Student s1=new Student();
-			s1.setStudentName("Kanika");
+			s1.setStudentName("Satya Nadella");
 			s1.setRollNo(14);
 			s1.setMarks(78);
 			s1.setStd("X");
-			s1.setAddress("Block D, Kalindi Colony, New Delhi, Delhi - 118876");
+			s1.setAddress("Block D, Microsoft, Redmond, CA");
 			String tblName="StudentTable1";
-			boolean flag=jof.create(tblName);
-			if(flag) {
-				System.out.println("Table Created Successfully");
+			try{
+				boolean flag=jof.create(tblName);
 			}
-			else {
+			catch(Exception e) {
 				System.out.println("Table not created");
 			}
-			jof.save(s1, tblName);
+			System.out.println("Table Created Successfully");
+			try{
+				jof.save(s1, tblName);
+			}
+			catch(Exception e) {
+				System.out.println("Unable to Save");
+			}
 			Student s2=new Student();
-			s2.setStudentName("Naina");
+			s2.setStudentName("Sundar Pichai");
 			s2.setRollNo(2);
 			s2.setMarks(93);
 			s2.setStd("XII");
-			s2.setAddress("Block A, Lajpat Nagar - III , New Delhi, Delhi - 110024");
-			jof.save(s2, tblName);
+			s2.setAddress("Block A, Google , Mountain View, CA");
+			try{
+				jof.save(s2, tblName);
+			}
+			catch(Exception e) {
+				System.out.println("Unable to Save");
+			}
 			Student s3=new Student();
-			s3.setStudentName("Saniya");
+			s3.setStudentName("Larry Page");
 			s3.setRollNo(9);
 			s3.setMarks(83);
 			s3.setStd("XI");
-			s3.setAddress("Block F, Sarojni Nagar , New Delhi, Delhi - 110989");
-			jof.save(s3, tblName);
-			ResultSet rs=jof.select("*", tblName);
-			System.out.println("Table Data is :-");
-			while(rs.next()) {
-				System.out.println(rs.getString(1)+"   "+rs.getInt(2)+"   "+rs.getInt(3)+"   "+rs.getString(4)+"   "+rs.getString(5));
+			s3.setAddress("Block F, Google, Mountain View, CA");
+			try{
+				jof.save(s3, tblName);
 			}
+			catch(Exception e) {
+				System.out.println("Unable to Save");
+			}
+			ArrayList<Object> list=jof.select("*", tblName);
+			System.out.println("Table Data is :-");
+			int k=0;
+			for(int i=0;i<list.size();i++) {
+				k++;
+				System.out.print(list.get(i)+"   ");
+				if(k==5&&i!=0) {
+					System.out.println();
+					k=0;
+				}
+			}
+			System.out.println();
 			String name="Daisy";
 			int a=jof.update(name, s2.getRollNo(), tblName);
 			System.out.println(a+" records updated");
-			rs=jof.select("*", tblName);
+			list=jof.select("*", tblName);
 			System.out.println("Table Data is :-");
-			rs=jof.select("*", tblName);
-			while(rs.next()) {
-				System.out.println(rs.getString(1)+"   "+rs.getInt(2)+"   "+rs.getInt(3)+"   "+rs.getString(4)+"   "+rs.getString(5));
+			k=0;
+			for(int i=0;i<list.size();i++) {
+				k++;
+				System.out.print(list.get(i)+"   ");
+				if(k==5&&i!=0) {
+					System.out.println();
+					k=0;
+				}
 			}
+			System.out.println();
 			a=jof.delete(s3.getRollNo(), tblName);
 			System.out.println(a+" records deleted successfully");
-			rs=jof.select("*", tblName);
+			list=jof.select("*", tblName);
 			System.out.println("Table Data is :-");
-			while(rs.next()) {
-				System.out.println(rs.getString(1)+"   "+rs.getInt(2)+"   "+rs.getInt(3)+"   "+rs.getString(4)+"   "+rs.getString(5));
+			k=0;
+			for(int i=0;i<list.size();i++) {
+				k++;
+				System.out.print(list.get(i)+"   ");
+				if(k==5&&i!=0) {
+					System.out.println();
+					k=0;
+				}
 			}
+			System.out.println();
 			jof.closeIt();
 			System.out.println("Connection Closed Successfully");
 		}
-		catch(ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		catch(SQLException e) {
-			e.printStackTrace();
+		catch(Exception e) {
+			System.out.println("Test JDBC ORM Framework Exception");
 		}
 	}
 }
