1249d6443708edd9291f9fd3157cd4a25a973559
Syed Mohd Altamash <44392139+smaltamash@users.noreply.github.com>
2019-08-17 13:20:16 +0530

    Update DBController.java

--- src/com/jdbc/orm/DBController.java
+++ src/com/jdbc/orm/DBController.java
@@ -1,13 +1,13 @@
 package com.jdbc.orm;
 
-import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 
 public interface DBController {
 	void createConnection(String connectivity, String url, String uName, String pass)throws ClassNotFoundException, SQLException;
 	boolean create(String tblName)throws SQLException;
-	ResultSet select(String columns, String tblName)throws SQLException;
+	ArrayList<Object> select(String columns, String tblName)throws SQLException;
 	int insert(String sql)throws SQLException;
 	int update(String name, int rollNo, String tblName)throws SQLException;
 	int delete(int rollNo, String tblName)throws SQLException;
